a77deb31ae76a2ba3c9666b845b1e4f0e5c15326
gmdbook <george@directbooker.com>
2025-10-16 13:06:41 -0400

    removing commented code

--- src/directbooker/types.ts
+++ src/directbooker/types.ts
@@ -1,13 +1,3 @@
-// export interface HotelType {
-//   hotel_id: number;
-//   name: string;
-//   carousel_image: string;
-//   description?: string;
-//   price: string;
-//   price_link: string;
-//   rating: number;
-// }
-
 export interface Hotel {
   hotel_id?: number | undefined;
   property_token: string;
